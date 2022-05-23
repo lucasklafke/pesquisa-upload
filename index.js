04d0323397e334b16d0a40505492c63367bae735
Lucas klafke <lucasnkz11@gmail.com>
2022-05-23 16:20:50 -0300

    last

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-import express from "express"
+import express, {json}from "express"
 import multer from "multer"
+import dotenv from "dotenv"
+import cors from "cors"
 const app = express()
+
+dotenv.config()
+
+app.use(cors())
+app.use(json())
 
 const storage = multer.diskStorage({
         destination: (req,file,cb) =>{
@@ -15,12 +22,28 @@ const upload = multer({storage})
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
-app.post("/", upload.single('img'), (req,res) =>{
+app.post("/", upload.single('file'), async (req,res) =>{
         console.log("body",req.body)
         console.log("file",req.file)
         res.send("ok")
 })
 
-app.get("/",(req,res) => res.render("home"))
+app.get("/", async(req,res) => {
+
+        let db;
+        const mongoClient = new MongoClient(process.env.MONGO_URL);
+        try {
+                await mongoClient.connect();
+                db = mongoClient.db("uploadtestes");
+                console.log("Connected to uploadtestes mongoDB database");
+
+                const testes = db.collection("testes")
+
+                const imagesTestes = await testes.findMany({})
+
+        } catch (e) {
+                console.log("Failed to connect to uploadtestes mongoDB database", e);
+        }
+})
 
 app.listen(5000)
